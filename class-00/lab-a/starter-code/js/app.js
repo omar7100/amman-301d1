187d1bbcb8e0fc4284b4573c32448a87759fcaf4
omar7100 <omar.s.alshoul@gmail.com>
2019-11-10 20:49:22 +0200

    lab00-a

--- class-00/lab-a/starter-code/js/app.js
+++ class-00/lab-a/starter-code/js/app.js
@@ -6,13 +6,13 @@ const leftImage = document.getElementById('left');
 const centerImage = document.getElementById('center');
 const rightImage = document.getElementById('right');
 
-const allProducts = [];
+let allProducts = [];
 const container = document.getElementById('image_container');
 const viewed = [];
 const labels = [];
 const pics = [leftImage, centerImage, rightImage];
 const list = document.getElementById('productlist');
-const totalClicks = 0;
+let totalClicks = 0;
 const views = [];
 const votes = [];
 
@@ -62,10 +62,10 @@ function handleClick(event) {
     showList();
     makeChart();
   }
-  for(const i = 0; i < names.length; i++){
+  for(let i = 0; i < names.length; i++){
     if(event.target.id === allProducts[i].name) {
       allProducts[i].votes += 1;
-      console.log(event.target.id + ' has ' + allProducts[i].votes + ' votes in ' + allProducts[i].views + ' views');
+      console.log(`${event.target.id }  has   ${allProducts[i].votes}   votes in   ${allProducts[i].views}   views `);
     }
   }
   localStorage.busmall = JSON.stringify(allProducts);
@@ -74,11 +74,9 @@ function handleClick(event) {
 }
 
 function showList() {
-  for(const i = 0; i < allProducts.length; i++) {
-    let l
-  iEl = document.createElement('li');
-    liE
-  l.textContent = allProducts[i].name + ' has ' + allProducts[i].votes + ' votes in ' + allProducts[i].views + ' views';
+  for(let i = 0; i < allProducts.length; i++) {
+    const liEl = document.createElement('li');
+    liEl.textContent = `${allProducts[i].name}   has   ${allProducts[i].votes}   votes in   ${allProducts[i].views}   views `;
     list.appendChild(liEl);
   }
 }
